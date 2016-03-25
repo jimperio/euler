12de5697596bed5d944f88d4a55ffaa52b3b4370
Jomel Imperio <jimperio@gmail.com>
2016-03-25 16:36:35 +0800

    Add data and solution for Problem 22

---
+++ prob022.py
@@ -0,0 +1,29 @@
+"""
+Using `names.txt` (saved here as `prob022.txt`), a 46K text file containing
+over five-thousand first names, begin by sorting it into alphabetical order.
+Then working out the alphabetical value for each name, multiply this value by
+its alphabetical position in the list to obtain a name score.
+
+For example, when the list is sorted into alphabetical order, COLIN, which is
+worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN
+would obtain a score of 938 x 53 = 49714.
+
+What is the total of all the name scores in the file?
+"""
+
+def parse_names(source='prob022.txt'):
+  with open(source, 'r') as f:
+    data = f.read()
+  names = map(lambda s: s.strip('"'), data.split(','))
+  return names
+
+def get_value(name):
+  return sum(map(lambda c: ord(c) - 64, name))
+
+if __name__ == "__main__":
+  names = parse_names()
+  names.sort()
+  total = 0
+  for i, name in enumerate(names):
+    total += (i + 1) * get_value(name)
+  print total
